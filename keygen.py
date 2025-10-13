3728ee80aed379979fafcd6b3cb90823720a9696
Selim Yahia-Messaoud <57829200+LostBlackbox@users.noreply.github.com>
2025-10-13 02:20:28 +0100

    Update keygen to have unique keys

--- keygen.py
+++ keygen.py
@@ -1,10 +1,24 @@
+import crud
+
 import random
 import string
 
-def generate_random_key(size: int):
+from sqlalchemy.orm import Session
+
+
+def generate_random_key(size: int = 6):
     """
     Generate a random key of a given size.
     There are 62 possible characters so 62^size possible keys.
     """
     chars = string.ascii_letters + string.digits
     return "".join(random.choices(chars, k=size))
+
+def generate_unique_random_key(db: Session) -> str:
+    """
+    Generate a random key that is unique in the database.
+    """
+    key = generate_random_key()
+    while crud.get_db_url_by_key(db, key):
+        key = generate_random_key()
+    return key
